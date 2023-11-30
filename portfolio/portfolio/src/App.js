4cef3d464463643e50359e941c96b08b124517d4
Ömer Atlı <atliomer41@gmail.com>
2023-11-30 16:36:11 +0300

    Loading Eklendi
    Loading animasyonu eklendi.

--- portfolio/portfolio/src/App.js
+++ portfolio/portfolio/src/App.js
@@ -1,24 +1,38 @@
+import React, { useState, useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
+import Loading from "./components/Loading";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Portfolio/>
-      <Experience/>
-      <Contact/>
-      <SocialLinks/>
+      {loading && <Loading />}
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
     </div>
   );
 }
 
 export default App;
+
  
